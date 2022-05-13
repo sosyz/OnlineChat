4e81b2b514b39fda9d58372a61aea65475c00a60
Sonui <me@sonui.cn>
2022-05-13 23:26:00 +0800

    feat: 雪花ID

---
+++ src/main/java/cn/sonui/onlinechat/utils/IDGenderConfig.java
@@ -0,0 +1,23 @@
+package cn.sonui.onlinechat.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class IDGenderConfig {
+
+    //数据中心[0,31]
+    @Value("${snowflake.datacenterId}")
+    private long datacenterId;
+
+    //机器标识[0,31]
+    @Value("${snowflake.machineId}")
+    private long machineId;
+
+    @Bean
+    public SnowFlakeFactory getSnowFlakeFactory() {
+        SnowFlakeFactory snowFlakeFactory = new SnowFlakeFactory(datacenterId,machineId);
+        return snowFlakeFactory;
+    }
+}
