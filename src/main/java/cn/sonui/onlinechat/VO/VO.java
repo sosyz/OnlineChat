4c6d7c52fe3356143ef8c487faa32489b3504a61
Sonui <me@sonui.cn>
2022-05-18 00:01:31 +0800

    feat: 添加VO包

---
+++ src/main/java/cn/sonui/onlinechat/VO/VO.java
@@ -0,0 +1,27 @@
+package cn.sonui.onlinechat.VO;
+
+public interface VO {
+    /**
+     * 获取错误码
+     * @return 错误码
+     */
+    Integer getCode();
+
+    /**
+     * 设置错误码
+     * @param code 错误码
+     */
+    void setCode(Integer code);
+
+    /**
+     * 获取响应信息
+     * @return 响应信息
+     */
+    String getMsg();
+
+    /**
+     * 设置响应信息
+     * @param message 响应信息
+     */
+    void setMsg(String message);
+}
