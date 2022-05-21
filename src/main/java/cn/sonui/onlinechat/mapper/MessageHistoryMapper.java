e4466a1716fd92c81e80ced3f6996e08f6d8ae52
Sonui <me@sonui.cn>
2022-05-21 22:08:12 +0800

    feat: 聊天消息模型

---
+++ src/main/java/cn/sonui/onlinechat/mapper/MessageHistoryMapper.java
@@ -0,0 +1,41 @@
+package cn.sonui.onlinechat.mapper;
+
+import cn.sonui.onlinechat.model.MessageHistory;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+
+/**
+ * @author Sonui
+ */
+@Repository
+public interface MessageHistoryMapper {
+    /**
+     * 插入消息
+     *
+     * @param message 待插入的消息
+     * @return 消息ID
+     */
+    @Insert("INSERT INTO `messages` " +
+            "(`sender`, `receiver`, `type`, `content`, `create_time`, `status`) " +
+            "VALUES " +
+            "(#{sender}, #{receiver}, #{type}, #{content}, NOW(), #{status})")
+    @Result(column = "create_time", property = "createTime", javaType = Date.class)
+    @Options(useGeneratedKeys = true,keyProperty = "id",keyColumn = "id")
+    int insertMessage(MessageHistory message);
+
+    /**
+     * 查询消息
+     *
+     * @param id 消息id
+     * @return 消息
+     */
+    @Select("SELECT * FROM `messages` WHERE `id` = #{id}")
+    MessageHistory selectMessage(Integer id);
+
+
+}
---
+++ src/main/java/cn/sonui/onlinechat/model/MessageHistory.java
@@ -0,0 +1,87 @@
+package cn.sonui.onlinechat.model;
+
+import cn.sonui.onlinechat.utils.SnowFlakeFactory;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+
+/**
+ * @author Sonui
+ */
+public class MessageHistory {
+    private Long id;
+    private String content;
+    private Long sender;
+    private String receiver;
+    private Short type;
+    private Date createTime;
+    private Integer status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Long getSender() {
+        return sender;
+    }
+
+    public void setSender(Long sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public Short getType() {
+        return type;
+    }
+
+    public void setType(Short type) {
+        this.type = type;
+    }
+
+    public Date getCreatTime() {
+        return createTime;
+    }
+
+    public void setCreatTime(Date creatTime) {
+        this.createTime = creatTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
